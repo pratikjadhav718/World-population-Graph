6e0d62099e6668739ad194eff7caf3ecc46dd8dd
pratikjadhav718 <pratikjadhav718@gmail.com>
2022-02-12 23:18:01 +0530

    adding recharts in project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [seconds, setSeconds] = useState(1);
+  
+  useEffect(() => {
+    const timer = setInterval(() => { setSeconds(seconds + 1); }, 1000);               // clearing interval
+    return () => clearInterval(timer);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Number of seconds is {seconds}</h1>
     </div>
   );
 }
