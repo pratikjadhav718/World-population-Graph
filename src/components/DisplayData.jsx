a687af2638b7fe12275ba97e190776e72acb5235
pratikjadhav718 <pratikjadhav718@gmail.com>
2022-02-12 23:57:42 +0530

    adding dispay components

---
+++ src/components/DisplayData.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import data from "./Data/test.json";
+import { GraphData } from './DisplayGraph';
+
+export const DisplayData = () => {
+    const [seconds, setSeconds] = useState(1);
+    const [currYear, setCurrYear] = useState(1960);
+    const [allData, setAllData] = useState(data.Data);
+    const [datas, setDatas] = useState([]);
+
+    const filterTopTen = () => {
+
+        if (currYear == 2020) {
+            return;
+        }
+
+        let opArr = [];
+
+        for (let i = 0; i < 10; i++) {
+            // console.log(allData[i].CountryName, allData[i].CountryName, allData[i].Population[currYear.toString()], currYear);
+            let obj = {};
+            obj["CountryName"] = allData[i].CountryName;
+            obj["Population"] = allData[i].Population[currYear.toString()];
+            obj["currYear"] = currYear;
+            opArr.push(obj);
+        }
+
+        setDatas(opArr);
+
+        if (currYear < 2020) {
+            setCurrYear(currYear + 1);
+        }
+
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => { filterTopTen(); }, 100);               // clearing interval
+        return () => clearInterval(timer);
+    });
+    return (
+        <div>
+            <h1>Number of seconds is {seconds} and year is {currYear}</h1>
+
+            <GraphData arr={datas}/>
+
+        </div>
+    )
+}
